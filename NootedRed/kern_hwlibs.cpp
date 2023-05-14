feede15e1b3541a894445a29dfbd389483db78d5
Nyan cat <17372086+NyanCatTW1@users.noreply.github.com>
2023-05-14 22:09:16 +0800

    (DON'T USE) Try GC 9.2.1
    Signed-off-by: Nyan cat <17372086+NyanCatTW1@users.noreply.github.com>

--- NootedRed/kern_hwlibs.cpp
+++ NootedRed/kern_hwlibs.cpp
@@ -139,7 +139,7 @@ AMDReturn X5000HWLibs::wrapPspSwInit(uint32_t *inputData, void *outputData) {
     return ret;
 }
 
-uint32_t X5000HWLibs::wrapGcGetHwVersion() { return 0x090400; }
+uint32_t X5000HWLibs::wrapGcGetHwVersion() { return 0x090201; }
 
 void X5000HWLibs::wrapPopulateFirmwareDirectory(void *that) {
     auto *fwDir = getMember<void *>(that, 0xB8) = IOMallocZero(0xD8);

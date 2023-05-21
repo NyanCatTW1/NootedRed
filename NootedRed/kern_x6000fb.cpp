a04bc18934eba466892eaf3a3f838d39256c3f3f
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2023-05-21 17:30:35 +0800

    [no ci] Silence X6000FB wraps

--- NootedRed/kern_x6000fb.cpp
+++ NootedRed/kern_x6000fb.cpp
@@ -53,7 +53,7 @@ bool X6000FB::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_
                 this->orgFramebufferGetAttribute},
             {"__ZNK22AmdAtomObjectInfo_V1_421getNumberOfConnectorsEv", wrapGetNumberOfConnectors,
                 this->orgGetNumberOfConnectors},
-            {"__ZNK24AMDRadeonX6000_AmdLogger10dumpBufferE7LogType11LogSeverityPKvm", wrapDumpBuffer, orgDumpBuffer},
+            // {"__ZNK24AMDRadeonX6000_AmdLogger10dumpBufferE7LogType11LogSeverityPKvm", wrapDumpBuffer, orgDumpBuffer},
             {"__ZN30AMDRadeonX6000_AmdAgdcServices24vendor_doDeviceAttributeEjPmmS0_S0_P25IOExternalMethodArguments",
                 wrapVendorDoDeviceAttribute, orgVendorDoDeviceAttribute},
             {"_dm_logger_write", wrapDmLoggerWrite},
@@ -312,15 +312,15 @@ void X6000FB::wrapDumpBuffer(void *that, uint32_t logType, uint32_t logSeverity,
 IOReturn X6000FB::wrapVendorDoDeviceAttribute(void *that, uint32_t cmd, void *param2, uint64_t param3, void *param4,
     void *param5, void *param6) {
     static uint32_t callId = 1;
-    DBGLOG("x6000fb",
+    /*DBGLOG("x6000fb",
         "vendor_doDeviceAttribute call %u << (that: %p cmd: 0x%X param2: %p param3: 0x%llX param4: %p param5: %p "
         "param6: %p)",
         callId, that, cmd, param2, param3, param4, param5, param6);
-    NRed::i386_backtrace();
+    NRed::i386_backtrace();*/
     if (cmd == 0x1301) { NRed::sleepLoop("Calling orgVendorDoDeviceAttribute", 60000); }
     auto ret = FunctionCast(wrapVendorDoDeviceAttribute, callback->orgVendorDoDeviceAttribute)(that, cmd, param2,
         param3, param4, param5, param6);
-    DBGLOG("x6000fb", "vendor_doDeviceAttribute >> 0x%X", ret);
+    // DBGLOG("x6000fb", "vendor_doDeviceAttribute >> 0x%X", ret);
     callId++;
     return ret;
 }
